aa772d0fede32d87c58aeb9139ecd0fc4d8bead6
MateNaw <49524385+MateNaw@users.noreply.github.com>
2020-03-15 17:07:09 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./App.scss";
+
+class App extends Component {
+  render() {
+    return <h1>siemka</h1>;
+  }
+}
+
+export default App;
